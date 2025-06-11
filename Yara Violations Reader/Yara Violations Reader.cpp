4ca961ebc138862fee5d3d16153c2f898e43d702
NtReadVirtualMemory <83749737+NtReadVirtualMemory@users.noreply.github.com>
2025-06-11 22:10:30 +0200

    Update Yara Violations Reader.cpp

--- Yara Violations Reader/Yara Violations Reader.cpp
+++ Yara Violations Reader/Yara Violations Reader.cpp
@@ -43,8 +43,7 @@ int main()
     cursorInfo.bVisible = FALSE;
     SetConsoleCursorInfo(hConsole, &cursorInfo);
     while (true) {
-        // 0x2840F8 Next Update (version-38d71f95cf424504)
-        YaraResult results = Read<YaraResult>(hyperionBase + 0x2D7540); 
+        YaraResult results = Read<YaraResult>(hyperionBase + 0x2D64CC); 
         MoveCursorToTop();
 
         std::cout << "SCAN_BAD_CERT = " << results.SCAN_BAD_CERT << "          \n";
